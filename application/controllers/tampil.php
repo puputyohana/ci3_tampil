39fc6debf4c55873ec2e9ac7c0b309b35bf643de
puputyohana <71644046+puputyohana@users.noreply.github.com>
2021-03-30 19:13:01 +0700

    Update tampil.php

--- application/controllers/tampil.php
+++ application/controllers/tampil.php
@@ -15,4 +15,21 @@ class tampil extends CI_Controller {
 		$data['user'] = $this->tampil_model->getAllUsers();
 		$this->load->view('tampil_view', $data);
 	}
+	public function addnew(){
+		$this->load->view('addform.php');
+	}
+		public function insert(){
+		$pegawai['iduser'] = $this->input->post('iduser');
+		$pegawai['username'] = $this->input->post('username');
+		$pegawai['password'] = $this->input->post('password');
+		$pegawai['nama_lengkap'] = $this->input->post('nama_lengkap');
+		$pegawai['divisi'] = $this->input->post('divisi');
+		$pegawai['no_rek'] = $this->input->post('no_rek');
+
+		$query = $this->tampil_model->insertuser($pegawai);
+
+		if($query){
+			header('location:'.base_url().$this->index());
+		}
+	}
 }
