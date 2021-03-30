732aff2a13d2eeec6a98704906a212d0447bd297
puputyohana <71644046+puputyohana@users.noreply.github.com>
2021-03-30 19:13:52 +0700

    Update tampil_model.php

--- application/models/tampil_model.php
+++ application/models/tampil_model.php
@@ -8,5 +8,9 @@
 			$query =  $this->db->get('user');
 			return $query->result();
 		}
+		
+		public function insertuser($pegawai){ //insertuser harus sesuai di tampil.php
+			return $this->db->insert('user', $pegawai); //user adalah nama tabel di db
+		}
 	}
 ?>
